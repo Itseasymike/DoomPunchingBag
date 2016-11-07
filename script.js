a968617c81e340b2912fc27596cd6e48078aeb1c
Michael Rivera <mrivera.dev@gmail.com>
2016-11-07 15:19:30 -0500

    new updates

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
   "use strict";
-// perspective .rotate z
+
 (function() {
 
-var song = new Audio ("sounds/doom-song.mp3");
-  song.play();
-//Global Variables
-  // var keycodes = event.keycode;
-  // var body = $('body');
+// Some global variables
   var doAttack;
-  var $left = $(' #left');
-  var $right = $(' #right');
   var enemyHealth = 150;
   var damage = 10;
   var counter = 0;
   var scream = ["sounds/scream.mp3"];
-  var song = ["sounds/doom-song.mp3"];
 
+//Randomizes sounds made when enemy is hit
   var playList = function() {
-    var actions =  [
+  var actions =  [
     "sounds/gut-punch.mp3",
     "sounds/grunt.mp3",
     "sounds/slap.mp3",
@@ -27,71 +21,71 @@ var song = new Audio ("sounds/doom-song.mp3");
     "sounds/woosh.mp3",
   ]
 
- var randomNum = Math.floor(Math.random() * (actions.length));
+  var randomNum = Math.floor(Math.random() * (actions.length));
 
- // Used stack overflow post to figure out how to actually play sounds
-  //Also how to correctly add the audio elements to the DOM
-  // http://stackoverflow.com/questions/20227266/play-random-sounds-without-repeat
-    $("<audio autoplay><source src=\"" + actions[randomNum] + "\" type=\"audio/mpeg\"></audio>").css('display', 'none').append("body");
+// Used stack overflow post to figure out how to actually play sounds
+// Also how to correctly add the audio elements to the DOM
+// http://stackoverflow.com/questions/20227266/play-random-sounds-without-repeat
+      $("<audio autoplay><source src=\"" + actions[randomNum] + "\" type=\"audio/mpeg\"></audio>").css('display', 'none').append("body");
 
 }
 
 
-// INITIALIZES GAME
-var startGame = function() {
-  $(' #container ').css('display', 'block');
-  $(' #start-btn ').toggleClass('animated').addClass('fadeOut');
-  $(' #ehealth').text(enemyHealth);
+// Initializes game
+  var startGame = function() {
+      $(' #container ').css('display', 'block');
+      $(' #start-btn ').toggleClass('animated').addClass('fadeOut');
+      $(' #ehealth').text(enemyHealth);
 
-//ACTIVATES KEYDOWN PROPERTIES
+// Activates keydown properties for right and left keys
   $(document).keydown(function(event) {
     if (event.keyCode === 39 ) {
-    $(' #right ').animate({'top': '-=140px'}, 50)
-    $(' #right ').animate({'left': '-=40px'}, 50)
-    $(' #right ').animate({'top': '+=140px'}, 50)
-    $(' #right ').animate({'left': '+=40px'}, 50)
-    playList();
-    myAttack();
+      $(' #right ').animate({'top': '-=140px'}, 50)
+      $(' #right ').animate({'left': '-=40px'}, 50)
+      $(' #right ').animate({'top': '+=140px'}, 50)
+      $(' #right ').animate({'left': '+=40px'}, 50)
+        playList();
+          myAttack();
   }
  });
 
-$(document).keydown(function(event) {
+  $(document).keydown(function(event) {
     if ( event.keyCode === 37 ) {
-    $(' #left ').animate({'top': '-=120px'}, 50)
-    $(' #left ').animate({'left': '-=40px'}, 50)
-    $(' #left ').animate({'top': '+=120px'}, 50)
-    $(' #left ').animate({'left': '+=40px'}, 50)
-    playList();
-    myAttack();
+      $(' #left ').animate({'top': '-=120px'}, 50)
+      $(' #left ').animate({'left': '-=40px'}, 50)
+      $(' #left ').animate({'top': '+=120px'}, 50)
+      $(' #left ').animate({'left': '+=40px'}, 50)
+        playList();
+          myAttack();
   }
 });
 
 }
 
-//DECREASE ENEMY HEALTH TO ZERO
-//YOU WIN WHEN ENEMY IS DEFEATED
-var myAttack = function() {
+// Decrease enemy's health to zero to win
+  var myAttack = function() {
 
-  $(' #ehealth').text(enemyHealth);
-  counter ++;
+      $(' #ehealth').text(enemyHealth);
+        counter ++;
 
-     if (enemyHealth === enemyHealth) {
-        (enemyHealth -= damage);
+    if (enemyHealth === enemyHealth) {
+      $(' #ehealth').text(enemyHealth -= damage);
          console.log(enemyHealth);
          console.log(counter)
 
              }
-
-     if (enemyHealth  === 100) {
+// Progressively changes sprite depending on enemy's health
+// Displays 'YOU WIN!' if conditions are met
+    if (enemyHealth  === 100) {
       $(' .enemy ').toggleClass('phase1');
         console.log(enemyHealth);
     }
-     if (enemyHealth === 70) {
+    if (enemyHealth === 70) {
       $(' .enemy ').removeClass('phase1');
       $(' .enemy ').toggleClass('phase2');
     }
 
-     if (enemyHealth === 50) {
+    if (enemyHealth === 50) {
       $(' .enemy ').removeClass('phase2');
       $(' .enemy ').toggleClass('phase3');
     }
@@ -101,7 +95,7 @@ var myAttack = function() {
       $(' .enemy ').toggleClass('phase4');
     }
 
-      if (enemyHealth <= 0) {
+    if (enemyHealth <= 0) {
       $(' #ehealth').css('display', 'none');
       $(' #left ').css('display', 'none');
       $(' #right ').css('display', 'none');
@@ -118,9 +112,9 @@ var myAttack = function() {
     }
   }
 
-// ENEMY ONE HIT KILL AFTER 10 SECS
-// DISPLAYS YOU LOSE
-var oneHitKill = function() {
+// Enemy one-hit-kill after 10 seconds
+// Displays 'YOU LOSE!'
+  var oneHitKill = function() {
     doAttack = setTimeout(function(){
       $(' .enemy ').removeClass('phase4');
       $(' .enemy ').toggleClass('phase5');
@@ -131,59 +125,66 @@ var oneHitKill = function() {
       $(' #btn-right').css('display', 'none');
       $(' #container ').first().toggleClass('animated').addClass('shake');
       $(' #lose ').css('display', 'block').toggleClass('animated').addClass('bounceInDown');
-       $(' #replay').css('display', 'block');
-       $("<audio autoplay><source src=\"" + scream + "\" type=\"audio/mpeg\"></audio>").css('display', 'none').append("body");
-      console.log("You lose!");
+      $(' #replay').css('display', 'block');
+      $("<audio autoplay><source src=\"" + scream + "\" type=\"audio/mpeg\"></audio>").css('display', 'none').append("body");
+        console.log("You lose!");
      }, 10000);
 }
 
-var stopAtttack = function() {
+// Stops oneHitKill function if player wins game
+  var stopAtttack = function() {
     clearTimeout(doAttack);
 }
-
+// Reloads page
   var replayGame = function(){
     location.reload();
   }
-
- var  myEventListeners = function() {
-
-  $(' #replay ').click(function(event) {
-      replayGame();
+// Buttons
+  var  myEventListeners = function() {
+// Continues to play music
+      $(' #play-btn ').click(function(event) {
+      $(' #player ').trigger('play');
+      $(' #play-btn ').css('display', 'none');
+      $(' #stop-btn ').css('display', 'block');
+  });
+// Stops music from playing
+      $(' #stop-btn ').click(function(event) {
+      $(' #player ').trigger('pause');
+      $(' #stop-btn ').css('display', 'none');
+      $(' #play-btn ').css('display', 'block');
   });
 
-$(' #start-btn ').click(function(event) {
-    oneHitKill();
-      startGame();
-
-});
-$( "#target" ).keydown(function() {
-
-});
-
-//ON CLICK ANIMATES RIGHT ARM
-$(' #btn-right ').click(function(event) {
-    $(' #right ').animate({'top': '-=140px'}, 50)
-    $(' #right ').animate({'left': '-=40px'}, 50)
-    $(' #right ').animate({'top': '+=140px'}, 50)
-    $(' #right ').animate({'left': '+=40px'}, 50)
-      playList();
-      myAttack();
+      $(' #replay ').click(function(event) {
+        replayGame();
   });
-//ON CLICK ANIMATES LEFT ARM
-$(' #btn-left ').click(function(event) {
-    $(' #left ').animate({'top': '-=120px'}, 50)
-    $(' #left ').animate({'left': '-=40px'}, 50)
-    $(' #left ').animate({'top': '+=120px'}, 50)
-    $(' #left ').animate({'left': '+=40px'}, 50)
-      playList();
-      myAttack();
+
+      $(' #start-btn ').click(function(event) {
+        oneHitKill();
+          startGame();
+});
+// On click animates right arm
+      $(' #btn-right ').click(function(event) {
+      $(' #right ').animate({'top': '-=140px'}, 50)
+      $(' #right ').animate({'left': '-=40px'}, 50)
+      $(' #right ').animate({'top': '+=140px'}, 50)
+      $(' #right ').animate({'left': '+=40px'}, 50)
+        playList();
+          myAttack();
+  });
+// On click animates left arm
+      $(' #btn-left ').click(function(event) {
+      $(' #left ').animate({'top': '-=120px'}, 50)
+      $(' #left ').animate({'left': '-=40px'}, 50)
+      $(' #left ').animate({'top': '+=120px'}, 50)
+      $(' #left ').animate({'left': '+=40px'}, 50)
+        playList();
+          myAttack();
   });
 
 }
 
-  $(document).ready(function(){
-    // $("<audio autoplay><source src=\"" + song + "\" type=\"audio/mpeg\"></audio>").css('display', 'none').append("body");
-    myEventListeners();
+      $(document).ready(function(){
+        myEventListeners();
   })
 
 })();
